d7fb8349bd256d8e52cec6afad0b9b485db33e70
Emily Boegheim <20103559@tafe.wa.edu.au>
2023-06-17 14:45:57 +0800

    Begin to implement CarPark unit tests (draft)

---
+++ tests/test_simple_mqtt_carpark.py
@@ -0,0 +1,29 @@
+import unittest
+from smartpark.simple_mqtt_carpark import CarPark
+
+class TestCarPark(unittest.TestCase):
+    def setUp(self):
+        self.carpark = CarPark('../config/tiny_carpark.toml')
+
+    def test_cars_entering(self):
+        self.carpark.on_car_entry()
+        self.carpark.on_car_entry()
+        self.assertEqual(2, self.carpark.total_cars)
+
+    def test_cars_exiting(self):
+        self.carpark.on_car_entry()
+        self.carpark.on_car_entry()
+        self.carpark.on_car_entry()
+        self.carpark.on_car_exit()
+        self.carpark.on_car_exit()
+        self.assertEqual(1, self.carpark.total_cars)
+
+    def test_available_spaces(self):
+        self.carpark.on_car_entry()
+        self.assertEqual(1, self.carpark.available_spaces)
+    
+    def test_available_spaces_not_negative(self):
+        self.carpark.on_car_entry()
+        self.carpark.on_car_exit()
+        self.carpark.on_car_exit()
+        self.assertEqual(0, self.carpark.available_spaces)
